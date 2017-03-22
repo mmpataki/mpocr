198456a68cba7feca5cc054a1b676b42531e28d0
mmp <mmp@madbox>
2017-03-23 03:27:57 +0530

    Updated Segmented.java

--- src/mpocr/Segmentation.java
+++ src/mpocr/Segmentation.java
@@ -1,13 +1,7 @@
 package mpocr;
 
 class Segmentation {
-
-<<<<<<< HEAD
 	static Segment[] segmentImage(OImage oi) {
             return null;
         }
-=======
-	static Segment[] segmentImage(OImage oi);
->>>>>>> ad02b1d78a8fa102df5b4c619b078e7d74fb6e0a
-
 }
