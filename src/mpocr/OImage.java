286e65ffa1dd453d60700d3b0e2147a1ea1d9841
AnarghyaJ <jantalianarghya@gmail.com>
2017-04-08 22:55:39 +0530

    Updated Binarization
    Check if Binarization is working properly

--- src/mpocr/OImage.java
+++ src/mpocr/OImage.java
@@ -1,5 +1,6 @@
 package mpocr;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -10,7 +11,7 @@ import javax.imageio.ImageIO;
 class OImage extends BasicImage {
 
     /* where the image is stored */
-    private BufferedImage img;
+    private static BufferedImage img;
     private boolean binarized;
 
     public OImage(String imgpath) {
@@ -46,20 +47,111 @@ class OImage extends BasicImage {
     
     /* to binarize the image */
     public void binarize() {
-        for (int i = 1; i < getHeight(); i++) {
-            for (int j = 1; j < getWidth(); j++) {
-                int c = iData[i][j];
-                int r = (c & 0xff);
-                int g = (c & 0xff00) >> 8;
-                int b = (c & 0xff0000) >> 16;
-                iData[i][j] = (((r + g + b) / 3) > 128) ? 0 : -1;
+        
+        /* first we need to convert image to grayscale*/
+        grayscale(img);
+        exportImage("grayscale.jpg");
+        int red;
+        int newPixel;
+        int threshold = Treshold( );
+        
+        for(int i=0; i<img.getWidth(); i++) {
+        for(int j=0; j<img.getHeight(); j++) {
+ 
+            // Get pixels
+            red = new Color(img.getRGB(i, j)).getRed();
+            int alpha = new Color(img.getRGB(i, j)).getAlpha();
+            if(red > threshold) {
+                newPixel = 255;
+            }
+            else {
+                newPixel = 0;
+            }
+            newPixel = colorToRGB(alpha, newPixel, newPixel, newPixel);
+            img.setRGB(i, j, newPixel); 
+ 
+        }
+    }
+        
+ binarized = true;
+ exportImage("binarized.jpg");
+    }
+    private static int colorToRGB(int alpha, int red, int green, int blue) {
+ 
+        int newPixel = 0;
+        newPixel += alpha;
+        newPixel = newPixel << 8;
+        newPixel += red; newPixel = newPixel << 8;
+        newPixel += green; newPixel = newPixel << 8;
+        newPixel += blue;
+ 
+        return newPixel;
+ 
+    }
+    private static int Treshold( ) {
+     int[] histogram = imageHistogram();
+    int total = img.getHeight() * img.getWidth();
+ 
+    float sum = 0;
+    for(int i=0; i<256; i++) sum += i * histogram[i];
+ 
+    float sumB = 0;
+    int wB = 0;
+    int wF = 0;float varMax = 0;
+    int threshold = 0;
+ 
+    for(int i=0 ; i<256 ; i++)
+    {
+        wB += histogram[i];
+        if(wB == 0) continue;
+        wF = total - wB;
+ 
+        if(wF == 0) break;
+ 
+        sumB += (float) (i * histogram[i]);
+        float mB = sumB / wB;
+        float mF = (sum - sumB) / wF;
+ 
+        float varBetween = (float) wB * (float) wF * (mB - mF) * (mB - mF);
+ 
+        if(varBetween > varMax) 
+        {
+            varMax = varBetween;
+            threshold = i;
+        }
+    }
+ 
+    return threshold;}
+    
+    
+    
+    private static int[] imageHistogram() 
+     
+    {
+ 
+        int[] histogram = new int[256];
+ 
+        for(int i=0; i<histogram.length; i++) histogram[i] = 0;
+ 
+        for(int i=0; i<img.getWidth(); i++) {
+            for(int j=0; j<img.getHeight(); j++) {
+                int red = new Color(img.getRGB (i, j)).getRed();
+                histogram[red]++;
             }
         }
-        binarized = true;
+ 
+        return histogram;
+ 
     }
 
     /* export image to a file */
     public void exportImage(String path) {
+        try {
+            File ProcessedImage=new File(path);
+            ImageIO.write(img, "jpg", ProcessedImage);
+        } catch (IOException ex) {
+            Logger.getLogger(OImage.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
 
@@ -67,5 +159,27 @@ class OImage extends BasicImage {
     public void invertScale() {
 
     }
+    
+    public void grayscale(BufferedImage Image)
+    {
+        for(int i=0;i<getWidth();i++){
+          for(int j=0;j<getHeight();j++){
+              //Using awt Colors
+               Color c = new Color(Image.getRGB(j, i));
+                int red = (int)(c.getRed() * 0.2126);
+               int green = (int)(c.getGreen() * 0.7152);
+               int blue = (int)(c.getBlue() *0.0722);
+               Color newColor = new Color(red+green+blue,red+green+blue,red+green+blue);
+              Image.setRGB(j,i,newColor.getRGB());
+          }
+        }
+        
+    }
+
 
 }
+        
+    
+
+
+
